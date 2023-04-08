7f900bf8c16a0bf173376f92275f20c40c7bc4fb
GaoChang <gaochang@192.168.0.102>
2023-04-08 21:50:03 +0800

    matrix

--- app/src/main/java/com/chang/dailylearn/MainActivity.kt
+++ app/src/main/java/com/chang/dailylearn/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.chang.dailylearn
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.chang.dailylearn.databinding.ActivityMainBinding
+import com.chang.dailylearn.view.AnimatorMaskTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
+
+
 }
